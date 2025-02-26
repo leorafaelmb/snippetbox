6dab043668eb607cef90f1d237e4b25d49356edf
20leo02 <lrm2188@columbia.edu>
2025-02-26 10:16:18 -0500

    Parse and render home template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
+	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 	w.Write([]byte("Hello from Snippetbox"))
 
 }
