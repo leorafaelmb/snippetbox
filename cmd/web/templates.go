fba791d50b00c5e4a2eea9bdc2ede3d66ad0e5ba
20leo02 <lrm2188@columbia.edu>
2025-03-06 18:15:34 -0500

    Rewrite time formatting method

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -17,7 +17,7 @@ func humanDate(t time.Time) string {
 	if t.IsZero() {
 		return ""
 	}
-	return t.UTC().Format("02 Jan 2006 at 15:04")
+	return t.Format("02 Jan 2006 at 15:04")
 }
 
 var functions = template.FuncMap{
