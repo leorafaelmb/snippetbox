81ffe2fd211f1fff227f984f13f3a58bc45da5fe
20leo02 <lrm2188@columbia.edu>
2025-03-06 16:42:41 -0500

    Create template.FuncMap object containing custom humanDate() function

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"path/filepath"
 	"snippetbox.leorafaelmb.net/internal/models"
+	"time"
 )
 
 type templateData struct {
@@ -38,4 +39,12 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		cache[name] = ts
 	}
 	return cache, nil
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
