1157333fd249a904eac2df66ece2630c506108cc
20leo02 <lrm2188@columbia.edu>
2025-03-06 18:13:03 -0500

    Change ParseFiles target from page to base template

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -13,6 +13,17 @@ type templateData struct {
 	Snippets    []models.Snippet
 }
 
+func humanDate(t time.Time) string {
+	if t.IsZero() {
+		return ""
+	}
+	return t.UTC().Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
@@ -23,15 +34,11 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
 
-		ts, err = template.ParseFiles("./ui/html/base.tmpl")
-		if err != nil {
-			return nil, err
-		}
 		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
 		if err != nil {
 			return nil, err
@@ -44,15 +51,4 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		cache[name] = ts
 	}
 	return cache, nil
-}
-
-func humanDate(t time.Time) string {
-	if t.IsZero() {
-		return ""
-	}
-	return t.UTC().Format("02 Jan 2006 at 15:04")
-}
-
-var functions = template.FuncMap{
-	"humanDate": humanDate,
 }
