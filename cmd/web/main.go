47727dc7604c09cbdda97aa62cf2898291e04649
20leo02 <lrm2188@columbia.edu>
2025-03-17 17:46:37 -0400

    Add session manager, make it available to handlers via application struct

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,21 +4,24 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
+	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
+	_ "github.com/go-sql-driver/mysql"
 	"html/template"
 	"log/slog"
 	"net/http"
 	"os"
 	"snippetbox.leorafaelmb.net/internal/models"
-
-	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 type application struct {
-	logger        *slog.Logger
-	snippets      *models.SnippetModel
-	templateCache map[string]*template.Template
-	formDecoder   *form.Decoder
+	logger         *slog.Logger
+	snippets       *models.SnippetModel
+	templateCache  map[string]*template.Template
+	formDecoder    *form.Decoder
+	sessionManager *scs.SessionManager
 }
 
 func main() {
@@ -43,11 +46,17 @@ func main() {
 	}
 
 	formDecoder := form.NewDecoder()
+
+	sessionManager := scs.New()
+	sessionManager.Store = mysqlstore.New(db)
+	sessionManager.Lifetime = 12 * time.Hour
+
 	app := &application{
-		logger:        logger,
-		snippets:      &models.SnippetModel{DB: db},
-		templateCache: templateCache,
-		formDecoder:   formDecoder,
+		logger:         logger,
+		snippets:       &models.SnippetModel{DB: db},
+		templateCache:  templateCache,
+		formDecoder:    formDecoder,
+		sessionManager: sessionManager,
 	}
 
 	logger.Info("starting server", "addr", *addr)
