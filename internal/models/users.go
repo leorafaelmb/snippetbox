7594a722af1937ccadf03ec29b59098cc3a55b9b
20leo02 <lrm2188@columbia.edu>
2025-03-18 13:30:49 -0400

    Define functions for User DB operations

--- internal/models/users.go
+++ internal/models/users.go
@@ -16,3 +16,15 @@ type User struct {
 type UserModel struct {
 	DB *sql.DB
 }
+
+func (m *UserModel) Insert(name, email, password string) error {
+	return nil
+}
+
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	return 0, nil
+}
+
+func (m *UserModel) Exists(id int) (bool, error) {
+	return false, nil
+}
