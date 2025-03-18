5b4fc3cda98a278fce0fd831db9b7fd347cfacb4
20leo02 <lrm2188@columbia.edu>
2025-03-18 13:28:56 -0400

    Create users.go. Define User struct and UserModel struct

---
+++ internal/models/users.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+type User struct {
+	ID             int
+	Name           string
+	Email          string
+	HashedPassword []byte
+	Created        time.Time
+}
+
+type UserModel struct {
+	DB *sql.DB
+}
