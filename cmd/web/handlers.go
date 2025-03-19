fdc9b3d8a30ce30c91e0c94eada883c2b67a36e2
20leo02 <lrm2188@columbia.edu>
2025-03-19 03:30:47 -0400

    Update postUserLogout to logout a user

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -206,5 +206,12 @@ func (app *application) postUserLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) postUserLogout(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Logout the user...")
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully.")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
