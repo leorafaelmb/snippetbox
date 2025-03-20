cb79d69ecd7a920b5183cbff8b23a98bc26cd347
20leo02 <lrm2188@columbia.edu>
2025-03-20 15:29:49 -0400

    Create ping handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -215,3 +215,7 @@ func (app *application) postUserLogout(w http.ResponseWriter, r *http.Request) {
 	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully.")
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
+func ping(w http.ResponseWriter, r http.Request) {
+	w.Write([]byte("OK"))
+}
