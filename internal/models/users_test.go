86d7cfecb6dbcb9657f77424bca37ef241e5fd6a
20leo02 <lrm2188@columbia.edu>
2025-03-26 13:49:50 -0400

    Check for presence of -short flag, skip if present

--- internal/models/users_test.go
+++ internal/models/users_test.go
@@ -6,6 +6,10 @@ import (
 )
 
 func TestUserModelExists(t *testing.T) {
+	if testing.Short() {
+		t.Skip("models: skipping integration test")
+	}
+
 	tests := []struct {
 		name   string
 		userID int
