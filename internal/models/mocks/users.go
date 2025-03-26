b43e53e0057d2026079d877c7f5fa224b6e4f508
20leo02 <lrm2188@columbia.edu>
2025-03-26 11:56:56 -0400

    Add Insert method to UserModel mock

--- internal/models/mocks/users.go
+++ internal/models/mocks/users.go
@@ -1,3 +1,14 @@
 package mocks
 
+import "snippetbox.leorafaelmb.net/internal/models"
+
 type UserModel struct{}
+
+func (m *UserModel) Insert(name, email, password string) error {
+	switch email {
+	case "dupe@example.com":
+		return models.ErrDuplicateEmail
+	default:
+		return nil
+	}
+}
