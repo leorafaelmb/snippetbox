388609bae9ae9c57cb847ac2e40a0311f7964e0a
20leo02 <lrm2188@columbia.edu>
2025-03-26 12:21:55 -0400

    Create StringContains method

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -11,3 +11,11 @@ func Equal[T comparable](t *testing.T, actual, expected T) {
 		t.Errorf("got: %v; want %v", actual, expected)
 	}
 }
+
+func StringContains(t *testing.T, actual, expectedSubstring string) {
+	t.Helper()
+
+	if !string.Contains(actual, expectedSubstring) {
+		t.Errorf("got: %q; expected to contain %q", actual, expectedSubstring)
+	}
+}
