9ef3e75bff19f3eaf416536c105d6343966575d6
20leo02 <lrm2188@columbia.edu>
2025-03-26 11:52:46 -0400

    Create mock of models.SnippetModel. Add Insert method

---
+++ internal/models/mocks/snippets.go
@@ -0,0 +1,20 @@
+package mocks
+
+import (
+	"snippetbox.leorafaelmb.net/internal/models"
+	"time"
+)
+
+var mockSnippet = models.Snippet{
+	ID:      1,
+	Title:   "An old silent pond",
+	Content: "An old silent pond...",
+	Created: time.Now(),
+	Expires: time.Now(),
+}
+
+type SnippetModel struct{}
+
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	return 2, nil
+}
